d9888a7b86a10209355f4b5c6ed26c5ac5d0e6c9
UnionUnllimited <159059519+UnionUnllimited@users.noreply.github.com>
2026-02-27 15:43:47 +0300

    Update FRP_URL to point to the correct script

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ TMP_SCRIPT="/tmp/gh_script_installer.sh"
 # ------------------------------------------------------------
 PASSWALL_URL="https://raw.githubusercontent.com/UnionUnllimited/routeratl/refs/heads/main/paswall.sh"
 ZAPRET_URL=""
-FRP_URL=""
+FRP_URL="https://raw.githubusercontent.com/UnionUnllimited/routeratl/refs/heads/main/frp.sh"
 TIME_URL=""
 ADMIN_URL="https://raw.githubusercontent.com/UnionUnllimited/routeratl/refs/heads/main/adminpanel.sh"
 
