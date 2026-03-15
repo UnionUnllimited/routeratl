e1739fd59c65fffa51a60bd0aa1b7234649cec71
UnionUnllimited <159059519+UnionUnllimited@users.noreply.github.com>
2026-03-15 17:12:27 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,3 +3,11 @@ cat > /etc/rc.button/reset <<'EOF'
 exit 0
 EOF
 chmod +x /etc/rc.button/reset
+opkg install zram-swap
+uci set system.@system[0].zram_comp_algo='lz4'
+uci set system.@system[0].zram_size_mb='128'
+uci commit system
+/etc/init.d/zram enable
+/etc/init.d/zram start
+free -m
+cat /proc/swaps
